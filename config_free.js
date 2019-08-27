4ad40731508c1fad6a08678a6c42fe02aeb2c687
Nikolaj Villefrance <nvma14@student.aau.dk>
2019-08-27 15:58:51 +0200

    Revert "Remove Watermark and "free version""
    This reverts commit a8a68e2a6b7a049ae5a3915256a55c36f098488c.

--- config_free.js
+++ config_free.js
@@ -17,6 +17,10 @@ function watermark(svg) {
 
 const max_channel_imbalance = 5;
 
+const max_compare = 2;
+const disallow_target = true;
+const premium_html = "<h2>You gonna pay for that?</h2><p>To use target curves, or more than two graphs, upgrade to Patreon <a href='https://www.patreon.com/join/crinacle/checkout?rid=3775534'>Silver tier</a>.<p>";
+
 const targets = [
     { type:"Neutral"   , files:["Crinacle","Diffuse Field","Etymotic","Free Field","Innerfidelity ID"] },
     { type:"Preference", files:["Harman","Rtings","Sonarworks"] }
