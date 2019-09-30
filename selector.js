6a4901e099335c2fe048c7a83ae2fb2809cc3212
Nikolaj Villefrance <nvma14@student.aau.dk>
2019-09-30 11:36:50 +0200

    Update selector.js

--- selector.js
+++ selector.js
@@ -669,7 +669,7 @@ d3.json(DIR+"phone_book.json").then(function (brands) {
         .call(setClicks(setBrand));
 
     var bg = (h,fn) => function (p) {
-        d3.select(this).style("background", #303030);
+        d3.select(this).style("background", fn(p));
         (p.objs||[p]).forEach(q=>hl(q,h));
     }
     var phoneSel = d3.select("#phones").selectAll()
